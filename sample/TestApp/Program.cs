cfedc95d577c1d02b44e45a17b7620ed25e94dd6
Alexander Köplinger <alex.koeplinger@outlook.com>
2014-12-29 21:18:21 +0000

    Got TextBox working

--- sample/TestApp/Program.cs
+++ sample/TestApp/Program.cs
@@ -5,31 +5,34 @@ namespace TestApp
 {
     public class Form1 : Form
     {
-        public Form1()
+        public Form1 ()
         {
             var b = new Button();
             b.Text = "Click me";
             b.Click += (s, e) => Console.WriteLine("Hello from WinForms!");
 
-            var t = new Label();
-            t.Text = "This is a label";
-            t.Location = new System.Drawing.Point(50, 100);
+            var l = new Label();
+            l.Text = "This is a label";
+            l.Location = new System.Drawing.Point(50, 100);
+
+            var t = new TextBox();
+            t.Text = "This is a textbox";
+            t.Location = new System.Drawing.Point(100, 50);
 
             this.Controls.Add(b);
+            this.Controls.Add(l);
             this.Controls.Add(t);
         }
     }
 
     public class Program
     {
-        public void Main(string[] args)
+        public void Main (string[] args)
         {
-            Console.WriteLine("Hello from KRE");
+            Console.WriteLine("Hello from KRE!");
 
             Application.EnableVisualStyles();
             Application.Run(new Form1());
-            
-            Console.ReadLine();
         }
     }
 }
