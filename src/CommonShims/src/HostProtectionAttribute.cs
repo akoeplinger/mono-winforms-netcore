ffd0ff2e8162554c2484e69ec1d3cf7a8501c6ba
Alexander Köplinger <alex.koeplinger@outlook.com>
2014-12-30 18:32:54 +0000

    Added real MaskedTextProvider

---
+++ src/CommonShims/src/HostProtectionAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Security.Permissions
+{
+    public class HostProtectionAttribute : Attribute
+    {
+        public HostProtectionAttribute ()
+        {
+
+        }
+
+        public bool SharedState { get; set; }
+    }
+}
--- src/CommonShims/src/MaskedTextResultHint.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System.Windows.Forms
-{
-    public class MaskedTextResultHint
-    {
-    }
-}
