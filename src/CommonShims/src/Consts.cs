42bd9c59b281a9e8e6ae2ccf1f539984c4d417ed
Alexander Köplinger <alex.koeplinger@outlook.com>
2014-12-31 13:37:24 +0100

    Added missing string to Consts.cs

--- src/CommonShims/src/Consts.cs
+++ src/CommonShims/src/Consts.cs
@@ -7,4 +7,5 @@
 	public const string AssemblySystem_Drawing_Design = "System.Drawing.Design, Version=" + FxVersion + ", Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
 	public const string AssemblySystem_Design = "System.Design, Version=" + FxVersion + ", Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
 	public const string AssemblySystem_Windows_Forms = "System.Windows.Forms, Version=" + FxVersion + ", Culture=neutral, PublicKeyToken=b77a5c561934e089";
+	public const string AssemblySystem_Web = "System.Web, Version=" + FxVersion + ", Culture=neutral, PublicKeyToken=b77a5c561934e089";
 }
